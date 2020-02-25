baf651d90d0fa8a2aa4d77365b678e8bc9a91a16
vinc86 <vincenzomancuso86@gmail.com>
2020-02-25 14:55:51 +0100

    db setup

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+
+//db
+const mongoose = require('mongoose');
+const dbUrl = 'mongodb+srv://vincenzo:test@cluster0-uostm.mongodb.net/test?retryWrites=true&w=majority';
+const param = { useUnifiedTopology: true, useNewUrlParser: true }
 
 //initialize express app
 const port = 3000;
 const app = express();
+
+const product = require('./routes/product.route');
+app.use('/products', product);
+
+
+
+//connect to db
+
+mongoose.connect(dbUrl, param, ()=>{
+    console.log("Connected to db");
+});
 
 app.listen(3000, (err)=>{
     if(err){
---
+++ controllers/product.controller.js
@@ -0,0 +1,7 @@
+const Product = require('../models/product.model');
+
+exports.test =function (req,res){
+    res.send('Controller test...');
+};
+
+
