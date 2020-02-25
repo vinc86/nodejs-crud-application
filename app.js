22e100afff784ea3bb0b28271fa9a2736a355023
vinc86 <vincenzomancuso86@gmail.com>
2020-02-25 12:03:05 +0100

    server connection

--- app.js
+++ app.js
@@ -3,5 +3,12 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 //initialize express app
-
+const port = 3000;
 const app = express();
+
+app.listen(3000, (err)=>{
+    if(err){
+        console.error(`Error connection on port ${port}: `, err);
+    } 
+    console.log(`Listening on port: ${port}`);
+})
